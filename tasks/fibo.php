fd63fe22155b991a5db15e7c43012db064b41db6
ValleraFTM <sagdacov@mail.ru>
2022-02-25 10:28:48 +0300

    task1 + прочее

---
+++ tasks/fibo.php
@@ -0,0 +1,31 @@
+<?php
+
+// функция checkFibo проверяет, является 
+// ли число $num числом фибоначчи 
+function  checkFibo(int $num)
+{
+    $n1 = (5*($num**2)-4)**0.5;
+    $n2 = (5*($num**2)+4)**0.5;
+    
+    if ((fmod($n1, 1)) == 0 or (fmod($n2, 1)) == 0) return true;    
+    else return false; 
+}
+
+// функция прохождения по массиву
+function countNum(array $arr)
+{
+    $summ =0;
+    foreach($arr as $elem)
+    {
+        // проверка, является ли элемент массива подмассивом 
+        // и рекурсивный вызов функции прохода по массиву
+        if (is_array($elem)) $summ += countNum($elem);
+        elseif (checkFibo($elem)) $summ += $elem;
+    }
+    return $summ;
+}
+
+$arr = [8, [13, 21]];
+var_dump(countNum($arr));
+
+?>
